17e4c4d8f21742872a5991fd74d98c019b1792d0
Eric Frey <ericlfrey@gmail.com>
2024-02-12 21:10:29 -0600

    Rendered Home component on home screen. Passed loggedinuser as a prop

--- client/src/components/ApplicationViews.jsx
+++ client/src/components/ApplicationViews.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { AuthorizedRoute } from './auth/AuthorizedRoute';
 import Login from './auth/Login';
 import Register from './auth/Register';
+import Home from './Home';
 
 export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
   return (
@@ -12,7 +13,7 @@ export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
           index
           element={
             <AuthorizedRoute loggedInUser={loggedInUser}>
-              HomePage
+              <Home user={loggedInUser} />
             </AuthorizedRoute>
           }
         />
