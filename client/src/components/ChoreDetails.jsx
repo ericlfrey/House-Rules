7ebea675fb5fba2ec1024b0c05957705524bff4b
Eric Frey <ericlfrey@gmail.com>
2024-02-13 18:48:26 -0600

    Created a chore details component per the directions in the assignment

--- client/src/components/ChoreDetails.jsx
+++ client/src/components/ChoreDetails.jsx
@@ -1,16 +1,51 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { getChoreById } from '../managers/choreManager';
+import { Table } from 'reactstrap';
 
 export default function ChoreDetails() {
+  const [chore, setChore] = useState({});
   const { id } = useParams();
+
+  useEffect(() => {
+    getChoreById(id).then(setChore);
+  }, [id]);
+
+  const mostRecentCompletionDate = () => {
+    if (chore?.completions?.length) {
+      const mostRecent = chore.completions[chore.completions.length - 1];
+      const mostRecentDate = new Date(
+        mostRecent.completedOn
+      ).toLocaleDateString();
+      return mostRecentDate;
+    }
+  };
+
   return (
     <>
-      <h1>Chore Details Page {id}</h1>
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Frequency (days)</th>
+            <th>Difficulty (1 - 5)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>{chore.name}</th>
+            <td>{chore.choreFrequencyDays}</td>
+            <td>{chore.difficulty}</td>
+          </tr>
+        </tbody>
+      </Table>
+      <h5>Most Recent Completion - {mostRecentCompletionDate()}</h5>
+      <h5>Current Assignees:</h5>
+      {chore.completions?.map(c => (
+        <p key={c.id}>
+          {c.userProfile.firstName} {c.userProfile.lastName}
+        </p>
+      ))}
     </>
   );
 }
-
-// Create a component called ChoreDetails that shows the details for a chore
-// along with a list of the current assignees and the most recent completion.
-// This component should be viewable only by admins.
-// Add "Details" links to the ChoresList items,
-// but conditionally render them based on whether the loggedInUser is an Admin (you will have already created the logic to do this for the delete button).
